fc6943755dfbf65fd6fe5e995f7185aa106c2efd
Badhon40 <cse12005018brur@gmail.com>
2025-04-09 20:41:43 +0600

    router home

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./Routes/Routes";
+import { RouterProvider } from "react-router-dom";
+ import router from "./Routes/Routes";
 // import { Provider } from "react-redux";
 // import { Store, persistor } from "./Redux/store";
 // import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
-import MainLayout from "./components/Layout/MainLayout";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <Provider store={Store}> */}
       {/* <PersistGate persistor={persistor}> */}
-        {/* <RouterProvider router={router} /> */}
+        <RouterProvider router={router} />
         <Toaster />
-        <MainLayout></MainLayout>
-      {/* </PersistGate> */}
+        
+      {/* </PersistGate>
     {/* </Provider> */}
   </StrictMode>
 );
