98961162733567ff1ee86abe4f88c868406c3370
Badhon40 <cse12005018brur@gmail.com>
2025-04-10 21:21:05 +0600

    'change

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-
 const About = () => {
   return (
     <section className="py-24 relative xl:mr-0 lg:mr-5 mr-0">
@@ -98,4 +95,3 @@ const About = () => {
 };
 
 export default About;
->>>>>>> f4baf10d15d92a50dc010f279710ff27d544bb11
