71ade0d77fe7c9187a17a14555c08596aa19fb2b
majid111 <abdullahalmajid81@gmail.com>
2025-04-12 19:23:53 +0600

    added main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,19 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 // import { RouterProvider } from "react-router-dom";
 // import { Provider } from "react-redux";
 // import { store, persistor } from "./Redux/store";
 // import { PersistGate } from "redux-persist/integration/react";
 // import { Toaster } from "sonner";
 // import router from "./Routes/Routes";
-createRoot(document.getElementById("root")!)
-  .render
-  // <StrictMode>
-  //   <Provider store={store}>
-  //     <PersistGate persistor={persistor}>
-  //       <RouterProvider router={router} />
-  //       <Toaster />
-  //     </PersistGate>
-  //   </Provider>
-  // </StrictMode>
-  ();
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {/*<Provider store={store}>*/}
+    {/*<PersistGate persistor={persistor}>*/}
+    {/*<RouterProvider router={router} />*/}
+    {/*<Toaster />*/}
+    {/*</PersistGate>*/}
+    {/*</Provider>*/}
+  </StrictMode>
+);
