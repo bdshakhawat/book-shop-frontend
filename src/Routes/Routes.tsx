49caf7b8d54f1194354c79f8399358224bd51f24
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-04-12 20:47:43 +0600

    conflict resolve

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,7 +3,6 @@ import MainLayout from "../components/Layout/MainLayout";
 import NotFoundPage from "../components/404/NotFound";
 import Home from "../components/Home/Home";
 import AllBooks from "../Pages/Books/All Books/AllBooks";
-import Reviews from "../components/Reviews/Reviews";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import About from "../Pages/About/About";
@@ -32,33 +31,25 @@ const router = createBrowserRouter([
       },
     ],
   },
-      {
-        path: "/login",
-        element: <Login></Login>,
-      },
-      {
-        path: "/register",
-        element: <Register></Register>,
-      },
-    ],
+  {
+    path: "/login",
+    element: <Login></Login>,
   },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+
   {
     path: "/admin",
     element: <DashboardLayout></DashboardLayout>,
-    children: routeGenerator(AdminRoutes)
+    children: routeGenerator(AdminRoutes),
   },
   {
     path: "/user",
     element: <DashboardLayout></DashboardLayout>,
-    children: routeGenerator(UserRoutes)
+    children: routeGenerator(UserRoutes),
   },
 ]);
 
-<<<<<<< HEAD
-    }
-])
-
-export default router
-=======
 export default router;
->>>>>>> 1050542ee0075b91f986e4c57eb93781bcdc3075
