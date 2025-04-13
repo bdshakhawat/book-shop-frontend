d25fe90cab86a03e3277a4ceaa2ae75ecb557a5e
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-04-13 09:51:50 +0600

    conflict resolve

--- src/Pages/User/UserDashboard.tsx
+++ src/Pages/User/UserDashboard.tsx
@@ -1,10 +1,32 @@
-
 const UserDashboard = () => {
   return (
-    <div className="text-2xl text-center">
-      user dashboard
-    </div>
-  )
-}
+    <div className="">
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-border">
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab"
+          aria-label="My Orders"
+        />
+        <div className="tab-content  bg-base-600 p-10">
+          Tab content 1
+        </div>
 
-export default UserDashboard
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab"
+          aria-label="Profile Setting"
+          defaultChecked
+        />
+        <div className="tab-content border-base-300 bg-base-100 p-10">
+          Tab content 2
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default UserDashboard;
--- src/components/Layout/DashboardLayout.tsx
+++ src/components/Layout/DashboardLayout.tsx
@@ -2,6 +2,7 @@ import { NavLink, Outlet } from "react-router-dom";
 import { sidebarItemsGenerator } from "../../Utils/sideBarGenerator";
 import AdminRoutes from "../../Routes/AdminRoutes";
 import UserRoutes from "../../Routes/UserRoute";
+import { IoHomeOutline } from "react-icons/io5";
 
 const userRole = {
   ADMIN: "admin",
@@ -9,7 +10,7 @@ const userRole = {
 };
 
 const DashboardLayout = () => {
-  const currentUserRole = "admin";
+  const currentUserRole = "user";
   let sidebarItem;
 
   switch (currentUserRole) {
@@ -26,26 +27,42 @@ const DashboardLayout = () => {
   return (
     <div className="max-w-[2520px]  mx-auto ">
       <div className="md:flex lg:flex ">
-        <div className=" lg:w-60   lg:min-h-screen text-black shadow-xl border-2">
+        <div className=" lg:w-60  lg:min-h-screen  text-black shadow-xl border-2">
           <div className="flex justify-center items-center mt-2  ">
-            <img className="w-[50%]"
+            <img
+              className="w-[50%]"
               src="https://i.ibb.co.com/gbXj6cx9/360-F-211078110-mttx-Edu3gs-Sb-MKajsy98-E4-M4-E5-RUCiuo-removebg-preview.png"
               alt=""
             />
           </div>
-          <ul className=" text-lg space-y-3 pt-8 px-2 text-center">
-            {sidebarItem?.map((item, index) => {
-              console.log(item);
-              if (!item) return null; // skip undefined
-              return (
-                <div key={index}>
-                  <li className="bg-gray-50 hover:bg-orange-50 hover:text-orange-500 hover:font-semibold px-4 py-2 rounded-md ">
-                    <NavLink to={`${item.label}`}>{item.key}</NavLink>
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
+          <div className="flex flex-col justify-between h-[calc(100vh-96px)]">
+            <ul className=" text-lg space-y-3 pt-8 px-2 text-center">
+              {sidebarItem?.map((item, index) => {
+                console.log(item);
+                if (!item) return null; // skip undefined
+                return (
+                  <div key={index}>
+                    <li className="bg-gray-50 hover:bg-orange-50 hover:text-orange-500 hover:font-semibold px-4 py-2 rounded-md ">
+                      <NavLink to={`${item.label}`}>{item.key}</NavLink>
+                    </li>
+                  </div>
+                );
+              })}
+            </ul>
+
+            <div className=" ">
+              <ul className="list-none text-lg space-y-3 pt-8 px-2 text-center">
+                <li className=" bg-gray-50 hover:bg-orange-50 hover:text-orange-500 hover:font-semibold px-4 py-2 rounded-md ">
+                  <NavLink
+                    className={"flex items-center justify-center"}
+                    to={`/`}
+                  >
+                    <IoHomeOutline size={20}></IoHomeOutline> Home
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
 
         <div className="flex-1 py-10 px-8">
