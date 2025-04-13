a8f42380fb73ebf0c5bde28f8fe31bec161437db
saifulislam106 <saifulislam106915@gmail.com>
2025-04-13 13:41:15 +0600

    review section render in home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,6 +14,7 @@ const Home = () => {
       {/* <Navbar></Navbar> */}
       <Banner></Banner>
       <NewArrival></NewArrival>
+      <Reviews></Reviews>
       {/* <Footer></Footer> */}
     </div>
   );
